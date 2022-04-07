675f68e66e9b139bb399ba26aee1cac20c8b9995
Jeremy Bourqui <jeremy.bourqui@heig-vd.ch>
2022-04-07 14:05:45 +0200

    test tracker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 //     r.keys().forEach(r);
 //   }
   
-  importAll(require.context('./components', false, /\.js$/));
+// importAll(require.context('./components', false, /\.js$/));
 
   import './lib/obs-websocket';
   import './lib/screen-capture';
